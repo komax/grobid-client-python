a0fe19a42ff4871369211112de87c78f28887ee3
Maximilian Konzack <maximilian.konzack@idiv.de>
2019-05-27 10:12:10 +0200

    Added a logging of errors to the client

--- grobid-client.py
+++ grobid-client.py
@@ -7,6 +7,7 @@ import argparse
 import time
 import concurrent.futures
 import glob
+import logging
 from client import ApiClient
 import ntpath
 
@@ -31,7 +32,12 @@ class grobid_client(ApiClient):
         config_json = open(path).read()
         self.config = json.loads(config_json)
 
-    def process(self, input, output, n, service, generateIDs, consolidate_header, consolidate_citations):
+    def process(self, input, output, n, service, generateIDs, consolidate_header, consolidate_citations, error_log=False):
+        print(error_log)
+
+        # Set up logger for errors if needed.
+        if error_log:
+            logging.basicConfig(filename='errors.log', level=logging.WARNING)
         batch_size_pdf = self.config['batch_size']
         pdf_files = []
 
@@ -39,21 +45,21 @@ class grobid_client(ApiClient):
             pdf_files.append(pdf_file)
 
             if len(pdf_files) == batch_size_pdf:
-                self.process_batch(pdf_files, output, n, service, generateIDs, consolidate_header, consolidate_citations)
+                self.process_batch(pdf_files, output, n, service, generateIDs, consolidate_header, consolidate_citations, error_log)
                 pdf_files = []
 
         # last batch
         if len(pdf_files) > 0:
-            self.process_batch(pdf_files, output, n, service, generateIDs, consolidate_header, consolidate_citations)
+            self.process_batch(pdf_files, output, n, service, generateIDs, consolidate_header, consolidate_citations, error_log)
 
-    def process_batch(self, pdf_files, output, n, service, generateIDs, consolidate_header, consolidate_citations):
+    def process_batch(self, pdf_files, output, n, service, generateIDs, consolidate_header, consolidate_citations, error_log):
         print(len(pdf_files), "PDF files to process")
         #with concurrent.futures.ThreadPoolExecutor(max_workers=n) as executor:
         with concurrent.futures.ProcessPoolExecutor(max_workers=n) as executor:
             for pdf_file in pdf_files:
-                executor.submit(self.process_pdf, pdf_file, output, service, generateIDs, consolidate_header, consolidate_citations)
+                executor.submit(self.process_pdf, pdf_file, output, service, generateIDs, consolidate_header, consolidate_citations, error_log)
 
-    def process_pdf(self, pdf_file, output, service, generateIDs, consolidate_header, consolidate_citations):
+    def process_pdf(self, pdf_file, output, service, generateIDs, consolidate_header, consolidate_citations, error_log):
         # check if TEI file is already produced 
         # we use ntpath here to be sure it will work on Windows too
         pdf_file_name = ntpath.basename(pdf_file)
@@ -100,12 +106,16 @@ class grobid_client(ApiClient):
         if status == 503:
             time.sleep(self.config['sleep_time'])
             return self.process_pdf(pdf_file, output)
-        elif status != 200:
-            print('Processing failed with error ' + str(status))
-        else:
+        if status == 200:
+            # Successful.
             # writing TEI file
             with io.open(filename,'w',encoding='utf8') as tei_file:
                 tei_file.write(res.text)
+        else:
+            print('Processing failed with error ' + str(status))
+            if error_log:
+                logging.warning('Failed to process %s with status %d', pdf_file, status)
+
 
 def test():
     client = grobid_client()
@@ -120,7 +130,8 @@ if __name__ == "__main__":
     parser.add_argument("--n", default=10, help="concurrency for service usage") 
     parser.add_argument("--generateIDs", action='store_true', help="generate random xml:id to textual XML elements of the result files") 
     parser.add_argument("--consolidate_header", action='store_true', help="call GROBID with consolidation of the metadata extracted from the header") 
-    parser.add_argument("--consolidate_citations", action='store_true', help="call GROBID with consolidation of the extracted bibliographical references") 
+    parser.add_argument("--consolidate_citations", action='store_true', help="call GROBID with consolidation of the extracted bibliographical references")
+    parser.add_argument("--error-log", default=False, action='store_true', help="create a log file for all pdfs which cannot be handled") 
 
     args = parser.parse_args()
 
@@ -138,12 +149,13 @@ if __name__ == "__main__":
     generateIDs = args.generateIDs
     consolidate_header = args.consolidate_header
     consolidate_citations = args.consolidate_citations
+    error_log = args.error_log
 
     client = grobid_client(config_path=config_path)
 
     start_time = time.time()
 
-    client.process(input_path, output_path, n, service, generateIDs, consolidate_header, consolidate_citations)
+    client.process(input_path, output_path, n, service, generateIDs, consolidate_header, consolidate_citations, error_log)
 
     runtime = round(time.time() - start_time, 3)
     print("runtime: %s seconds " % (runtime))
